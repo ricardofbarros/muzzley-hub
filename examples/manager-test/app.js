5098872e2f89f01a3cbe10a2a49b1a435a3187b5
Ricardo Barros <ricardofbarros@hotmail.com>
2015-04-17 17:18:26 +0100

    update examples

--- examples/manager-test/app.js
+++ examples/manager-test/app.js
@@ -1,21 +1,27 @@
 // Dependencies
 var muzzleyHub = require('muzzley-hub');
 var Manager = muzzleyHub.Manager;
-var requestHandler = require('./server/requestHandler');
 var providerBridge = require('./provider/bridge');
 var config = require('./config');
+var Channels = require('./models/Channels');
+var Credentials = require('./models/Credentials');
+var Subscription = require('./models/Subscription');
 
 // Create a manager and pass the configurations of the manager
 var manager = new Manager(config);
 
-// Register methods from the discovery process
-manager.method('channels', requestHandler.channels);
-manager.method('authenticate', requestHandler.authenticate);
-manager.method('authorization', requestHandler.authorization);
-manager.method('subscriptions', requestHandler.subscriptions);
+// Register methods for the discovery process
+manager.discovery.getAuth(providerBridge.getAuth);
+manager.discovery.getChannels(providerBridge.getChannels);
 
-manager.method('write', fu
-)
+// Register methods for the interaction
+manager.interaction.write(providerBridge.write);
+manager.interaction.read(providerBridge.read);
+
+// Register models
+manager.model(Channels);
+manager.model(Credentials);
+manager.model(Subscription);
 
 // Start the manager!
 manager.start();
